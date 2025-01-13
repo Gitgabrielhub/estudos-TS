d533826b7d5fbbe064a93ca20871bf2d84889045
gabriel <gabrielsillva356@gmail.com>
2025-01-13 14:48:08 -0300

    estudando interfaces

---
+++ dist/index3/index.js
@@ -0,0 +1,19 @@
+function preencherDados(dados) {
+    document.body.innerHTML = `
+        <div>
+            <h2>${dados.nome}</h2>
+            <p> R$${dados.price}</p>
+            <p>inclui teclado :${dados.teclado === true ? 'sim' : 'nao'}</p>
+        </div>
+    `;
+}
+preencherDados({
+    nome: 'computador',
+    price: 300,
+    teclado: true
+});
+preencherDados({
+    nome: 'notebook',
+    price: 3000,
+    teclado: false
+});
