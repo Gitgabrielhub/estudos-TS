a95b0a483987a2821e76bf39492d3669a71e4af3
gabriel <gabrielsillva356@gmail.com>
2025-01-14 15:24:02 -0300

    iniciando a estilização da aplicação

--- dist/consumindoApi/index.js
+++ dist/consumindoApi/index.js
@@ -1,25 +1,37 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 async function fetchProduct() {
     const API = await fetch('https://api.origamid.dev/json/notebook.json');
     const data = await API.json();
     mostrarNaTela(data);
 }
 function mostrarNaTela(data) {
-    document.body.innerHTML = `
-    <div>
-        <p>Nome do produto: ${data.nome}</p>
-        <p> preço do produto: ${data.preco}</p>
-        <p> preço do produto: ${data.descricao}</p>
-        <p> preço do produto: ${data.garantia}</p>
+    document.querySelector('#containerProducts').innerHTML = `
+    <div id="divProducts">
+        <div>
+            <p>Nome do produto: ${data.nome}</p>
+            <p> preço do produto: ${data.preco}</p>
+            <p> preço do produto: ${data.descricao}</p>
+            <p> preço do produto: ${data.garantia}</p>
+        </div>
+        <div>
+            <p>Nome do produto: ${data.nome}</p>
+            <p> preço do produto: ${data.preco}</p>
+            <p> preço do produto: ${data.descricao}</p>
+            <p> preço do produto: ${data.garantia}</p>
+        </div>
+        <div>
+            <p>Nome do produto: ${data.nome}</p>
+            <p> preço do produto: ${data.preco}</p>
+            <p> preço do produto: ${data.descricao}</p>
+            <p> preço do produto: ${data.garantia}</p>
+        </div>
     </div>
-    <div>
+    <div class="mt-5">
         <p>informacoes de garantia: Este produto possui garantia de ${data.garantia}</p>
         <p> Empresa fabricante: ${data.empresaFabricante.nome}</p>
         <p> Nacionalidade do fabricante: ${data.empresaFabricante.pais}</p>
         <p> preço do produto: ${data.empresaMontadora.nome}</p>
     </div>
 
-`;
+    `;
 }
 fetchProduct();
