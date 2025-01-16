89c22e0957ec6e0d46e5cb96aee9f55171af856e
gabriel <gabrielsillva356@gmail.com>
2025-01-16 20:28:13 -0300

    instanceOf

---
+++ dist/instaceof/index.js
@@ -0,0 +1,16 @@
+"use strict";
+class Produto {
+    nome;
+    preco;
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    precoReal() {
+        let color = "red";
+        let colorPrice = "yellow";
+        return document.body.innerHTML = `O livro é <span style="color:${color} "> ${this.nome}</span> que custa R$ <span style="color:${colorPrice} ">${this.preco}</span>`;
+    }
+}
+const livro = new Produto('a arte da guerra', 20.00);
+console.log(livro.precoReal());
