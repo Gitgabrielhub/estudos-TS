2811ceddd4a0020f8c527a5d3a40b8d45245c5e8
gabriel <gabrielsillva356@gmail.com>
2025-01-18 00:01:52 -0300

    generics

---
+++ dist/generics/index.js
@@ -0,0 +1,34 @@
+"use strict";
+// GENERICS é um tipo de padrão
+function retorno(a) {
+    return a;
+}
+//console.log(retorno('gabriel')) // infere o tipo de acordo com o tipo de dado passado na função
+//console.log(retorno(300)) // no caso o generics será do tipo number
+//console.log(retorno(true)) // aqui o retorno será um booleano
+//pode- se tambem atribuir um tipo quando for executar a função
+/* retorno<number>(30)
+retorno<string>("30")
+retorno<boolean>(true) */
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const fruits = ['banana', 'pera', 'macã', 'goiaba', 'mamao'];
+function primeirosCinco(lista) {
+    return lista.slice(1, 6);
+}
+function notNull(num) {
+    if (num !== null) {
+        return num;
+    }
+    else {
+        return null;
+    }
+}
+console.log(notNull('gabriel fonseca')?.toUpperCase());
+function TipoDado(a) {
+    const resultado = {
+        dado: a,
+    };
+    console.log(resultado);
+    return resultado;
+}
+TipoDado("teste");
